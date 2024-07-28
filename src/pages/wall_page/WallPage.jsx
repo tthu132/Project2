7593541bba2cc501817d0832bd69b2f238371d95
tthu132 <tthu13222@gmail.com>
2024-07-29 01:49:22 +0700

    create layout

---
+++ src/pages/wall_page/WallPage.jsx
@@ -0,0 +1,12 @@
+function WallPage() {
+    return (
+        <div className="bg-black  text-white ">
+            <p>walll</p>
+            <p>walll</p>
+
+
+        </div>
+    );
+}
+
+export default WallPage;
